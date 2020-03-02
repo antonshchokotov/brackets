9a6020b1c7237e16d6f2bc4dbcd47765b84aa998
Anton Shchokotov <58229945+antonshchokotov@users.noreply.github.com>
2020-03-02 20:08:27 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.map(arr => arr.join(''));
+  let currentLength = 0;
+  do {
+    currentLength = str.length;
+    brackets.forEach(x => str = str.replace(x, ''));
+  } while (currentLength != str.length)
+  return !str.length;
 }
